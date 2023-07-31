a03aa12b11fd7e1f72efce484fdf6c6cf3d2da52
y1shin <134024716+R00t-get@users.noreply.github.com>
2023-07-31 15:47:41 +0800

    Update 16进制逆序_改.py
    增添了一个直接读取文件byte再修改的版本

--- 16进制逆序_改.py
+++ 16进制逆序_改.py
@@ -1,3 +1,18 @@
 normal_byte = open('flag.png','rb') #以二进制形式读取原文件的byte数据
 new_byte = open('new.png','wb') #打开一个文件，以二进制的形式写入byte数据
 new_byte = new_byte.write(normal_byte.read()[::-1]) 
+
+#other version
+file = open("Reverse.piz","rb").read()
+new_file = open("result1.zip","wb")
+
+result = b""
+for byte in file:
+    data = hex(byte)[2:].zfill(2)[::-1]
+    result += bytes.fromhex(data)
+new_file.write(result)
+
+'''
+原理是将源文件以byte形式读取，再将byte转换成hex
+将hex逆转后，再转换成byte形式写入
+'''
