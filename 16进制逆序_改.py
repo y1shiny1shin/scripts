e4eb4ca41b577d7f574fbe0d5ee8abd893313ff9
y1shin <134024716+R00t-get@users.noreply.github.com>
2023-07-31 16:09:28 +0800

    Update 16进制逆序_改.py
    提供文件逆向的两种思路

--- 16进制逆序_改.py
+++ 16进制逆序_改.py
@@ -1,7 +1,9 @@
+'''这个版本是完全逆转'''
 normal_byte = open('flag.png','rb') #以二进制形式读取原文件的byte数据
 new_byte = open('new.png','wb') #打开一个文件，以二进制的形式写入byte数据
 new_byte = new_byte.write(normal_byte.read()[::-1]) 
 
+'''这个版本是逐个逆转文件中的hex'''
 #other version
 file = open("Reverse.piz","rb").read()
 new_file = open("result1.zip","wb")
@@ -12,6 +14,7 @@ for byte in file:
     result += bytes.fromhex(data)
 new_file.write(result)
 
+print("[+]运行结束")
 '''
 原理是将源文件以byte形式读取，再将byte转换成hex
 将hex逆转后，再转换成byte形式写入
